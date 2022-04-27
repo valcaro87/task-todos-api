76a441d8b1fa806cc470ce9388312cd5001e03b7
valcaro87@gmail.com <valcaro87@gmail.com>
2022-04-27 09:50:11 +0800

    added next and previous ids

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,18 @@
 class Task < ApplicationRecord
+  include ActiveModel::Serialization
+
+  attr_accessor :previous_id
+  serialize :previous_id
+
+  attr_accessor :next_id
+  serialize :next_id
+
+  def next_id
+    Task.where("id > ?", self.id)&.first&.id || Task&.first&.id
+  end
+
+  def previous_id
+    Task.where("id < ?", self.id)&.last&.id || Task&.last&.id
+  end
+
 end
