612e54b702bae15a6b3100ec7023b80cc89583a4
valcaro87@gmail.com <valcaro87@gmail.com>
2022-08-07 01:12:02 +0800

    updated cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3000'
     resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put]
   end
+
+  allow do
+    origins 'https://task-todos-react.herokuapp.com'
+    resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put]
+  end
+
 end
