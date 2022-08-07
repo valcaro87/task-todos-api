fff16b13a64d756ace7c626b619f00bfc0b29cb4
valcaro87@gmail.com <valcaro87@gmail.com>
2022-08-07 16:46:13 +0800

    updated cors env prod vs dev

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,17 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put]
+
+  if Rails.env.development?
+    allow do
+      origins 'http://localhost:3000'
+      resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put]
+    end
   end
 
-  allow do
-    origins 'https://task-todos-react.herokuapp.com'
-    resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put]
+  if Rails.env.production?
+    allow do
+      origins 'https://task-todos-react.herokuapp.com'
+      resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put]
+    end
   end
 
 end
